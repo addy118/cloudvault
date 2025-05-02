7476309fcaad6b2850dacdb1f50b30998be4c5dc
addy <addyyy118@gmail.com>
2025-05-03 02:23:15 +0530

    chore: prod changes

--- src/axiosInstance.jsx
+++ src/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:3000",
-  baseURL: "https://cloudvaultt.netlify.app/",
+  // baseURL: "https://cloudvaultt.onrender.com/",
   withCredentials: true,
 });
 
